526a813fbccfbdf1db9b1b9a57ed89386960cca0
vicodes.dev <ajalasimon192@gmail.com>
2025-12-01 11:02:04 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const display = document.getElementById("calculator");
 
-const specialChars = ['%', '*', '+', "-", '/'];
+const specialChars = ["%", "*", "+", "-", "/", "="];
 
 function appendToCalculator(input) {
-  display.value += input;
-  
+  if (specialChars.includes(input) && display.value === "") {
+    return;
+  } else {
+    display.value += input;
+  }
 }
 
 function clearDisplay() {
@@ -12,9 +15,16 @@ function clearDisplay() {
 }
 
 function calculate() {
+  if (display.value.includes("%")) {
+    display.value = eval(display.value.replace("%", "/100"));
+  }
   try {
     display.value = eval(display.value);
   } catch (err) {
     display.value = "Error";
   }
+}
+
+function del() {
+  display.value = display.value.toString().slice(0, -1);
 }
